ace5a06768f48d1002b098be68b13be5d6810238
Matheus Rodrigues <matheus.silva47@estudante.ifms.edu.br>
2025-04-10 09:06:40 -0400

    Add files via upload

---
+++ kk.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+  int a, b, c;
+  //exercicio 3
+  printf("digite o valor da variável \n");
+    scanf("%d", &a);
+  printf("digite o valor da variável \n");
+  scanf("%d",&b);
+  printf("digite o valor da variável \n");
+    scanf("%d",&c);
+  printf("o valor da variavel a eh %d\n", a);
+  printf("o valor da variavel a eh %d\n", b);
+  printf("o valor da variavel a eh %d\n", c);
+  
+  return 0;
+}
