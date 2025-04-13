b19c5077e4b33affaa9635181388a83f3bf01271
Matheus Rodrigues <matheus.silva47@estudante.ifms.edu.br>
2025-04-13 12:09:42 -0300

    Add files via upload

---
+++ exercicio 6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+  int copo, agua, soma;
+  
+  printf("digite o primeiro valor: \n");
+  scanf("%d", &copo);
+  
+  printf("agora o segundo: \n"); 
+  scanf("%d", &agua);
+  
+  soma=copo + agua;
+  
+  printf("o resultado deu:%d", soma);
+  
+  
+  
+  return 0;
+  
+  }
---
+++ exercicio7.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(){
+ int x, y, x1, x2, soma1, soma2, dividir;
+
+printf("digite seu primeiro valor: \n", x);
+scanf("%d", &x);
+
+printf("digite seu segundo valor: \n", y);
+scanf("%d", &y);
+
+soma1=x+y;
+
+printf("a soma1 deu:%d\n", soma1);
+
+printf("digite seu terceiro valor: \n", x1);
+scanf("%d", &x1);
+
+printf("digite seu quarto valor: \n", x2);
+scanf("%d", &x2);
+
+soma2=x1+x2;
+
+printf("a soma2 deu:%d\n", soma2);
+
+dividir=soma1/soma2;
+
+printf("a divisão das duas somas dá:%d", dividir);
+
+
+return 0;
+
+}
