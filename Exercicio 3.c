d1aab953c4718dc3596dc263e5a18e12f508b787
Matheus Rodrigues <matheus.silva47@estudante.ifms.edu.br>
2025-04-13 16:36:48 -0300

    Update Exercicio 3.c

--- Exercicio 3.c
+++ Exercicio 3.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 
 int main(){
-
   int a, b, c;
   //exercicio 3. Considere um algoritmo que tenha como entrada 3 números inteiros chamados a, b, c
-  printf("digite o valor da variável \n");
+  
+  printf("digite o valor da variável a \n");
     scanf("%d", &a);
-  printf("digite o valor da variável \n");
-  scanf("%d",&b);
-  printf("digite o valor da variável \n");
+    
+  printf("digite o valor da variável b \n");
+    scanf("%d",&b);
+    
+  printf("digite o valor da variável c \n");
     scanf("%d",&c);
+    
   printf("o valor da variavel a eh %d\n", a);
-  printf("o valor da variavel a eh %d\n", b);
-  printf("o valor da variavel a eh %d\n", c);
+  printf("o valor da variavel b eh %d\n", b);
+  printf("o valor da variavel c eh %d\n", c);
   
   return 0;
 
