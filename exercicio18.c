f2f420260ec6bac20419b1f4f3a599c297a9b848
Matheus Rodrigues <matheus.silva47@estudante.ifms.edu.br>
2025-04-14 18:44:11 -0300

    Add files via upload

---
+++ exercicio18.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    char o=o;
+    char e=e;
+    printf("Digite a vogal 'O' maiuscula: ");
+    scanf(" %c", &o);
+    
+    printf("\nSua vogal se converteu na vogal 'E': ");
+    scanf("%c", &e);
+    
+    return 0;
+}
---
+++ exercicio19.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+    float x, y;
+    float multiplicar;
+    int resultado;
+    printf("Digite primeiro numero real: ");
+    scanf("%f", &x);
+    printf("Digite o segundo número real: ");
+    scanf("%f", &y);
+    
+    multiplicar=x*y;
+    resultado = multiplicar;
+    
+    printf("A multiplicacao desses dois valores deu:%d", resultado);
+    
+    
+    return 0;   
+}
