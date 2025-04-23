8576f9e6309f114621e417a5faecf2b9be4c02af
Matheus Rodrigues <matheus.silva47@estudante.ifms.edu.br>
2025-04-22 20:04:13 -0400

    Atualizar o exercicio18.c

--- exercicio18.c
+++ exercicio18.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-/*faça um algoritmo que responda o usuário quando ele digitar a vogal “o”com a vogal “e”*/
+/*faça um algoritmo que responda o usuário quando ele digitar a vogal “O”com a vogal “E”*/
 int main(){
     char o=o;
     char e=e;
