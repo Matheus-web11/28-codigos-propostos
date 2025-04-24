5ec44dd00b1ba4868b64f869c550f2a664bfd7e6
Matheus Rodrigues <matheus.silva47@estudante.ifms.edu.br>
2025-04-24 12:45:27 -0400

    Atualizar o exercicio21.c

--- exercicio21.c
+++ exercicio21.c
@@ -1,3 +1,6 @@
+/*crie um algoritmo que receba 2 números inteiros e realize um divisão.
+Em seguida informe se o resto dessa divisão é ímpar e imprima isso na tela. Nesse algoritmo é apresentado duas variaveis do tipo inteiro x e y e um float chamado divisao. eu peço que o usuario digite um numero inteiro e depois o outro. após isso, ele realizara uma divisão onde entra o float. só que como os valores digitados sao inteiros, e preciso converter esses numeros em float, depois eu faço if que se os numeros divididos darem resto 0 nao valida como resto impar, já se dividi dois numeros onde de resto 1, vai validar como impar e mostrara que a divisão saiu com resto impar*/
+
 #include<stdio.h>
 
 int main(){
@@ -5,8 +8,7 @@ int main(){
 int x, y;
 float divisao;
 
-/*crie um algoritmo que receba 2 números inteiros e realize um divisão.
-Em seguida informe se o resto dessa divisão é ímpar e imprima isso na tela.*/
+
     printf("Digite um valor inteiro qualquer: ");
     scanf("%d" , &x);
     printf("Digite seu segundo valor: ");
