3affae22d2d99993b8d0c9c7235599129da2972b
Matheus Rodrigues <matheus.silva47@estudante.ifms.edu.br>
2025-04-24 15:43:59 -0300

    Update exercicio03.c

--- exercicio03.c
+++ exercicio03.c
@@ -1,6 +1,7 @@
-#include <stdio.h>
 /*Considere um algoritmo que tenha como entrada 3 números inteiros. Tres variaveis onde cada uma vai apresentar um numero inteiro, onde após de ser digitado
 ele vai ser mostrado ao usuário*/
+
+#include <stdio.h>
 
 int main(){
   int a, b, c;
