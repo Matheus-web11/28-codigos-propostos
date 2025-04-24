1498197203c5b1131640196bf0db27e5050ae578
Matheus Rodrigues <matheus.silva47@estudante.ifms.edu.br>
2025-04-24 15:30:25 -0300

    Update exercicio13.c

--- exercicio13.c
+++ exercicio13.c
@@ -1,6 +1,10 @@
-#include <stdio.h>
-/*faça um algoritmo que colete a data de nascimento do usuário que devolva a sua idade em meses. Nesse algoritmo temos um tipo int com tres variaveis, chamado dia, mes e ano. e outra variavel idade em meses. onde eu vou pedir do usuario a data do seu nascimento, o mes e o ano. apos disso sera feito o processo de fazer o processo de saber qual é sua idade e fazer essa idade em meses. onde idade_em_meses sera feito para realizar uma operação que vai pegar o ano atual menos o ano que o usuario nasceu. depois vai pegar o valor e multiplicar pelo tanto de meses no ano que é 12. e depois vai fazer 4 menos o mes do usuario, o 4 é para definir que o valor sera calculado até o mes de abril.*/
+/*faça um algoritmo que colete a data de nascimento do usuário que devolva a sua idade em meses. Nesse algoritmo temos um tipo int com tres variaveis,
+chamado dia, mes e ano. e outra variavel idade em meses. onde eu vou pedir do usuario a data do seu nascimento, o mes e o ano. apos disso sera feito o processo de fazer o
+processo de saber qual é sua idade e fazer essa idade em meses. onde idade_em_meses sera feito para realizar uma operação que vai pegar o ano atual menos o ano que o usuario 
+nasceu. depois vai pegar o valor e multiplicar pelo tanto de meses no ano que é 12. e depois vai fazer 4 menos o mes do usuario, o 4 é para definir que o valor sera calculado
+até o mes de abril.*/
 
+#include <stdio.h>
 
 int main() {
 
