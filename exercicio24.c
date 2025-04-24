025ea50af6b6406080a6fda92374d9fef3aa446b
Matheus Rodrigues <matheus.silva47@estudante.ifms.edu.br>
2025-04-24 12:55:48 -0400

    Atualizar o exercicio24.c

--- exercicio24.c
+++ exercicio24.c
@@ -11,7 +11,7 @@ char defit;
 
 /*Crie um algoritmo que receba as seguintes informações de um cidadão:
  Idade, Sexo, Nacionalidade, Possui alguma deficiência grave? (Sim/Não).
- Com base nessas informações, o algoritmo deve informar se o cidadão está apto ou não apto ao alistamento militar obrigatório.*/
+ Com base nessas informações, o algoritmo deve informar se o cidadão está apto ou não apto ao alistamento militar obrigatório.. Nesse algoritmo eu criei um tipo inteiro para idade e depois 5 char, onde cada char fara uma questao do que sera pedido no alistamento. um para sexo masculino, outro para o Feminino,outro para validar o sexo do usuario usando o genero, outro pra nacao e o ultimo para deficiência. entao sera realizado as perguntas de forma sequencial. primeiro pede a idade do usuario sea idade do usuario for menor que 18 ele nao consegue prosseguir. já o outro, se o usuário for do genero feminino, nao consegue prosseguir porque o alistamento é so para homens. o outro é a ncionalidade se for brasileiro continua o processo, se nao for brasileiro ele sera retirado tambem do questionário porque o alistamento é específicamente para quem é brasileiro. e por ultimo eu peço se o usuário possui alguma deficiência se sim, ele nao pode prosseguir, mais se nao ele continuará o questionário e conseguira realizá-lo com sucesso*/
  
      printf("Seja bem-vindo ao processo seletivo do alistamento militar Brasileiro!\n");
      
