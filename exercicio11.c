e907e05e55d98e5c88ecc2d1c9915254c751a115
Matheus Rodrigues <matheus.silva47@estudante.ifms.edu.br>
2025-04-24 11:59:14 -0400

    Atualizar o exercicio11.c

--- exercicio11.c
+++ exercicio11.c
@@ -2,7 +2,7 @@
 
 int main() {
 
-//faça um algoritmo que insira dois números e que a divisão deles de um resultado que seja obrigatóriamente um número inteiro
+/*faça um algoritmo que insira dois números e que a divisão deles de um resultado que seja obrigatóriamente um número inteiro. Temos duas variaveis do tipo float, onde o usuario pode digitar dois numeros onde depois entra na variável int que se chama divisão, que vai pegar os valores digitados pelo o usuario e realizar a divisao deles, só que o resultado vai ser do tipo inteiro*/
 
 float nro1, nro2;
 int divisao;
