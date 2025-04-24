bf48dd6752ec4e0962926824e9d6c12813462759
Matheus Rodrigues <matheus.silva47@estudante.ifms.edu.br>
2025-04-24 17:31:06 -0300

    Atualizar o exercicio09.c

--- exercicio09.c
+++ exercicio09.c
@@ -1,3 +1,5 @@
+/*desenvolva um algoritmo que receba um número inteiro e informe se ele é par ou ímpar. Nesse algoritmo, foi usado uma variavel do tipo int chamado de nro, onde esse numero sera escolhido pelo o usuário, entao para fazer o processo de saber se é par ou ímpar vou usar o if. onde vou pegar o valor digitado do usuario e dividir por 2 onde, se o resto for zero ele sera par, se der resto ele sera impar*/
+
 #include <stdio.h>
 
 int main(){
