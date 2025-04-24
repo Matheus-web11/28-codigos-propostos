b2e3cadd7fb140937d812f4a88635f0591131a9f
Matheus Rodrigues <matheus.silva47@estudante.ifms.edu.br>
2025-04-24 17:44:36 -0300

    Atualizar o exercicio16.c

--- exercicio16.c
+++ exercicio16.c
@@ -1,3 +1,5 @@
+/*crie um algoritmo que tenha como entrada as 3 dimensões de um algoritmo em metros. Nesse algoritmo, eu faço 4 variaveis do tipo float, onde tres desses floats serao somados e darao o resultado dessa soma. vou pedir o valor da largura em metros, do comprimento em metros e a altura em metros. onde esses valores serao usados na operação aritmética, soma. entao a junção dessas tres dimensões dara ao usuário um so resultado da soma dos tres*/
+
 #include <stdio.h>
 
 int main(){
