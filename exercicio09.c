1de0d4f25fe2a7eb2c9c6d4db826c20cb49da03b
Matheus Rodrigues <matheus.silva47@estudante.ifms.edu.br>
2025-04-24 17:28:22 -0300

    Add files via upload

---
+++ exercicio09.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+int nro;
+
+printf("Digite um valor e vera se eh impar ou par:\n");
+scanf("%d", &nro);
+
+if(nro%2==00){
+  printf("Seu numero eh par!");}
+else{
+printf("Seu numero eh impar!");}
+
+return 0;
+}
