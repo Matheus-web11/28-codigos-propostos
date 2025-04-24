4010ad5413a2351ea8d9b7c33c88956360e0b9fe
Matheus Rodrigues <matheus.silva47@estudante.ifms.edu.br>
2025-04-24 08:03:44 -0400

    Add files via upload

---
+++ exercicio26.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int  main(){
+
+int a, b;
+int soma;
+
+printf("Digite dois valores inteiros ");
+scanf("%d" "%d", &a, &b);
+
+soma=a+b;
+
+if(soma>=10){
+  printf("O resultado da sua soma é maior ou igual a 10");}
+  else{
+  printf("Sua soma nao deu maior ou igual a 10!");}
+  
+  return 0;
+  }
+  
+
+
+
---
+++ exercicio27.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+float a;
+int resultado;
+
+printf("Digite um numero real: ");
+scanf("%f", &a);
+
+resultado = a;
+
+printf("Seu valor foi transformado em inteiro:%d",resultado);
+
+return 0;
+
+}
