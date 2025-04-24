662d2261dcfa5fec1a5b0e39906f5b9a2405d63c
Matheus Rodrigues <matheus.silva47@estudante.ifms.edu.br>
2025-04-24 19:44:53 -0300

    Update exercicio17.c

--- exercicio17.c
+++ exercicio17.c
@@ -1,4 +1,8 @@
-/*incompleto, não consegui terminar. faça um algoritmo que receba 5 números inteiros e retorne eles de forma ordenada e crescente. Nesse algoritmo, eu faço uma variavel onde pode armazenar 5 numeros em uma variavel chamada nro, e na frente do nome da variavel eu coloco o tanto de numeros que é pedido para ser colocado em ordem crescente, isso é chamado de vetor. e tem outra variavel chamado armazena, o nome ja diz tudo, essa variavel vai armazenar um valor para que esse valor seja organizado de forma crescente entao, se ele for o MENOR numero, ele espera que todos os números seja escritos para que ele entre na condição de ser o menor numero da sequencia, mesma coisa seria com o MAIOR, eu digito um valor, ele sera armazenado na variavel "armazena" e fica guardada, eu digito os restantes dos números, entao o computador le as condições para que possa organizar esse valor na ordem crescente*/
+/*incompleto, não consegui terminar. faça um algoritmo que receba 5 números inteiros e retorne eles de forma ordenada e crescente. Nesse algoritmo, eu faço uma variavel onde pode armazenar 5
+numeros em uma variavel chamada nro, e na frente do nome da variavel eu coloco o tanto de numeros que é pedido para ser colocado em ordem crescente, isso é chamado de vetor. e tem outra variavel chamado armazena,
+o nome ja diz tudo, essa variavel vai armazenar um valor para que esse valor seja organizado de forma crescente entao, se ele for o MENOR numero, ele espera que todos os números seja escritos para que ele entre na
+condição de ser o menor numero da sequencia, mesma coisa seria com o MAIOR, eu digito um valor, ele sera armazenado na variavel "armazena" e fica guardada, eu digito os restantes dos números, entao o computador le
+as condições para que possa organizar esse valor na ordem crescente*/
 
 #include <stdio.h>
 
