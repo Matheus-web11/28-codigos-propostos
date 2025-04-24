f3bfb130aaa196d5c909302548a06e396e98b7ea
Matheus Rodrigues <matheus.silva47@estudante.ifms.edu.br>
2025-04-24 15:31:10 -0300

    Update exercicio14.c

--- exercicio14.c
+++ exercicio14.c
@@ -1,10 +1,12 @@
+/*faça um algoritmo que declare dois valores reais e cinco carácteres. Nesse algoritmo foi feito um tipo float com duas variaveis x e y para os valores reais.
+e outro tipo char com 5 caracteres e outro para enter. onde esse char de 5 caracteres sera feito uma palavra, onde eu atribui valor oara cada caracter,
+e tambem fiz o mesmo processo com os float, eu escolhi dois valores reais para o float. o enter foi usado para que o usuario clique no enter e apareça a mensagem após.*/
+
 #include <stdio.h>
 
 int main() {
 
-/*faça um algoritmo que declare dois valores reais e cinco carácteres. Nesse algoritmo foi feito um tipo float com duas variaveis x e y para os valores reais. e outro tipo char com 5 caracteres e outro para enter. onde esse char de 5 caracteres sera feito uma palavra, onde eu atribui valor oara cada caracter, e tambem fiz o mesmo processo com os float, eu escolhi dois valores reais para o float. o enter foi usado para que o usuario clique no enter e apareça a mensagem após.*/
-
-float x, y;
+  float x, y;
 char c,a,r,o;
 char enter;
 
