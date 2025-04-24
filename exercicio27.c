17c4b90574d5ef74ef763011967d61ead4a86b9c
Matheus Rodrigues <matheus.silva47@estudante.ifms.edu.br>
2025-04-24 15:41:17 -0300

    Update exercicio27.c

--- exercicio27.c
+++ exercicio27.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-/*escreva um algoritmo que receba um valor em float e retorne um inteiro*/
+/*escreva um algoritmo que receba um valor em float e retorne um inteiro. Nesse algoritmo eu fiz uma variavel 'a' do tipo float, e outro do tipo inteiro 'resultado', o float
+'a' sera digitado
+pelo o usuario, onde esse valor sera convertido em inteiro, graças ao int 'resultado' que realizara o processo de float para inteiro*/
 
 int main(){
 
