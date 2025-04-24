db000b273a1274da9e00ece85e000708f88770fb
Matheus Rodrigues <matheus.silva47@estudante.ifms.edu.br>
2025-04-24 15:29:28 -0300

    Update exercicio11.c

--- exercicio11.c
+++ exercicio11.c
@@ -1,8 +1,10 @@
+/*faça um algoritmo que insira dois números e que a divisão deles de um resultado que seja obrigatóriamente um número inteiro.
+Temos duas variaveis do tipo float, onde o usuario pode digitar dois numeros onde depois entra na variável int que se chama divisão,
+que vai pegar os valores digitados pelo o usuario e realizar a divisao deles, só que o resultado vai ser do tipo inteiro*/
+
 #include <stdio.h>
 
 int main() {
-
-/*faça um algoritmo que insira dois números e que a divisão deles de um resultado que seja obrigatóriamente um número inteiro. Temos duas variaveis do tipo float, onde o usuario pode digitar dois numeros onde depois entra na variável int que se chama divisão, que vai pegar os valores digitados pelo o usuario e realizar a divisao deles, só que o resultado vai ser do tipo inteiro*/
 
 float nro1, nro2;
 int divisao;
