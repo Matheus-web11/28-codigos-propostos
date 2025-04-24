47ef1830a021fa74a00cfba38ca6da51814cf9ae
Matheus Rodrigues <matheus.silva47@estudante.ifms.edu.br>
2025-04-24 15:31:45 -0300

    Update exercicio15.c

--- exercicio15.c
+++ exercicio15.c
@@ -1,8 +1,10 @@
+/*crie um algoritmo que some 3 números inteiro e depois calcule a média. Nesse algoritmo sera feito a soma de tres valores digitados pelo o usuario,
+representado por x, y e z, tambem tem duas variaveis do tipo int, onde um e a soma e a divisao, que sera feito. tem tambem uma variavel do tipo float onde eu
+defini seu valor como 3, onde apos a soma dos tres valores vai realizar a media. onde vai pegar os valores inteiros e dividir por 3 e descobrir a media*/
+
 #include <stdio.h>
 
 int main() {
-
-/*crie um algoritmo que some 3 números inteiro e depois calcule a média. Nesse algoritmo sera feito a soma de tres valores digitados pelo o usuario, representado por x, y e z, tambem tem duas variaveis do tipo int, onde um e a soma e a divisao, que sera feito. tem tambem uma variavel do tipo float onde eu defini seu valor como 3, onde apos a soma dos tres valores vai realizar a media. onde vai pegar os valores inteiros e dividir por 3 e descobrir a media*/
 
 int x,y,z,soma, divisao;
 float s=3;
