bfe4003fb4d38c6900b6c0970a087dbc538ed84d
Matheus Rodrigues <matheus.silva47@estudante.ifms.edu.br>
2025-04-24 22:47:46 -0300

    Update exercicio06.c

--- exercicio06.c
+++ exercicio06.c
@@ -1,4 +1,5 @@
-/*algoritmo que tenha como entrada dois numeros inteiros que some e depois mostre o resultado. Possui duas variaveis onde cada um é o numero digitado pelo o usuário,
+/*algoritmo que tenha como entrada dois numeros inteiros que some e depois mostre o resultado. Possui duas variaveis onde
+cada um é o numero digitado pelo o usuário,
 a vai juntar esses dois valores e soma-los*/
 
 #include <stdio.h>
