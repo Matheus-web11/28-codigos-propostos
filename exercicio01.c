50bcebbc1c4e616ea9865d43cf0f8a55086fbb94
Matheus Rodrigues <matheus.silva47@estudante.ifms.edu.br>
2025-05-15 19:41:47 -0300

    Update exercicio01.c

--- exercicio01.c
+++ exercicio01.c
@@ -1,11 +1,3 @@
-/*Crie um algoritmo que identifica se o valor dado é um triângulo se for o algoritmo deve mostra se ele é equilátero, isósceles ou escaleno usando print, scanf, if e else.
-Nesse algoritmo, eu uso tres variáveis do tipo float, e peço pro usuario digitar os valores em metros que serao cada lado do meu triângulo, com isso, 
-eu preciso validar primeiro se os valores digitados pelo o usuário forma um triangulo, onde dois valores do triângulo precisa ser maior que o terceiro lado,
-caso a soma dos dois lados nao ser maior que o terceiro já invalida como triangulo. depois de eu confirmar que meus tres valores sao um triângulo. 
-eu preciso saber de qual tipo é esse triangulo, se é do tipo equilátero, isósceles ou escaleno. ele vai pegar os tres valores, se forem iguais sera validado
-como equilatero, se dois lados forem iguais e o outr diferente entra na validação do isosceles, já se os tres lados forem diferentes entra na validação do escaleno.
-e no final mostra se os valores forma um triangulo e qual tipo sera esse triangulo*/
-
 #include <stdio.h>
 #include <math.h>
 
