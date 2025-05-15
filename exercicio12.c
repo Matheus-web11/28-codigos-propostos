71e03172f69d4d9c5876d36f6ddad768f659396b
Matheus Rodrigues <matheus.silva47@estudante.ifms.edu.br>
2025-05-15 19:42:59 -0300

    Update exercicio12.c

--- exercicio12.c
+++ exercicio12.c
@@ -1,6 +1,3 @@
-/*faça um algoritmo com uma variável tipo char e defina o número de letras que a pessoa pode escrever. Aqui a gente possui uma variavel do tipo char
-seguidamente com o maximo de letras que o usuario pode digitar, aqui eu pedi que ele faça uma senha no maximo com 10 caracteres*/
-
 #include <stdio.h>
 
 int main() {
