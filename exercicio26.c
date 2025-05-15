a4e2c200aec0167881cf8860fb90f1bea7b59996
Matheus Rodrigues <matheus.silva47@estudante.ifms.edu.br>
2025-05-15 19:44:38 -0300

    Update exercicio26.c

--- exercicio26.c
+++ exercicio26.c
@@ -1,7 +1,3 @@
-/*faça um algoritmo que receba dois números inteiros e verifique se a soma deles é maior ou igual a 10. Nesse algoritmo eu usei dos tipos inteiro, e
-um int soma. os dois primeiros
-inteiros serao somados, o proprio usuario vai digita-los. a variavel soma vai fazer a operacao e vai verificar se a junção dos
-numeros inteiros vai dar um resultado maior que 10 ou igual*/
 #include <stdio.h>
 
 int  main(){
