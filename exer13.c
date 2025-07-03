624c37d5f1fd93218d8b13cb584077b8b55e00ff
Matheus Rodrigues <matheus.silva47@estudante.ifms.edu.br>
2025-07-03 20:35:57 -0300

    Update exer13.c

--- exer13.c
+++ exer13.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
+  int main()
+  {
+  int numeros[10] , i;
 
-int main(){
-
-int a1, b2, c3, d4, e5, f6, g7, h8, i9, j10;
-
-a1 = 10;
-b2 = 20;
-c3 = 30;
-d4 = 40;
-e5 = 50;
-f6 = 60;
-g7 = 70;
-h8 = 80;
-i9 = 90;
-j10 = 100;
-
-printf("%d %d %d %d %d %d %d %d %d %d" , a1, b2, c3, d4, e5, f6, g7, h8, i9, j10);
-
-return 0;
-}
+  printf("Digite 10 numeros inteiros: ");
+  
+  for(i = 0; i < 10; i++)
+    {
+    scanf("%d" , &numeros[i]);
+    }
+  printf("Seus valores sao: \n");
+  for(i = 0; i < 10; i++)
+    {
+    printf("%d " , numeros[i]);
+    }
+  return 0;
+  }
