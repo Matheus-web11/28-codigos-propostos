d3e723a5ef4abe416dc9c6f94fc1d02c3fdd0024
Matheus Rodrigues <matheus.silva47@estudante.ifms.edu.br>
2025-07-03 20:34:24 -0300

    Update exer08.c

--- exer08.c
+++ exer08.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
 
 int main(){
+  int ra;
+  int n1, n2, n3;
+    
+  printf("Digite seus primeiros digitos do RA: ");
+  scanf("%d" , &ra);
+  
+  if(ra > 100){
+    n1 = 9;
+    n2 = 8;
+    n3 = 7;
+    printf("Suas 3 notas sao: %d %d %d" , n1, n2, n3);}
 
-int ra;
-float n1, n2, n3
+  else{
+    n1 = 5;
+    n2 = 6;
+    n3 = 7;
+    printf("Suas 3 notas sao: %d %d %d" , n1, n2, n3);}
 
-ra = 108;
-n1 = 9;
-n2 = 8;
-n3 = 7;
-
-printf("%d %f %f %f" , ra, n1, n2, n3);
+printf("\nSeu RA eh: %d\n Sua nota 1 eh: %d\n Sua nota 2 eh: %d\n Sua nota 3 eh: %d\n" , ra, n1, n2, n3);
 
 return 0;
 }
