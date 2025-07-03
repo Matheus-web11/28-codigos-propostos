8e15a872d39e03d324d48b60582501f83af1ed7c
Matheus Rodrigues <matheus.silva47@estudante.ifms.edu.br>
2025-07-03 20:33:57 -0300

    Update exer07.c

--- exer07.c
+++ exer07.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
 
 int main(){
-  int nro1, nro2;
+  int ra;
+  int n1, n2, n3;
+    
+  printf("Digite seus primeiros digitos do RA: ");
+  scanf("%d" , &ra);
   
-  printf("Digite dois numeros inteiros: ");
-  scanf("%d %d" , &nro1, nro2);
+  if(ra > 100){
+    n1 = 9;
+    n2 = 8;
+    n3 = 7;
+    printf("Suas 3 notas sao: %d %d %d" , n1, n2, n3);}
 
-  printf("Seus valores forao: %d %d" , nro1, nro2);
+  else{
+    n1 = 5;
+    n2 = 6;
+    n3 = 7;
+    printf("Suas 3 notas sao: %d %d %d" , n1, n2, n3);}
 
-  return 0;
+printf("\nSeu RA eh: %d\n Sua nota 1 eh: %d\n Sua nota 2 eh: %d\n Sua nota 3 eh: %d\n" , ra, n1, n2, n3);
+
+return 0;
 }
