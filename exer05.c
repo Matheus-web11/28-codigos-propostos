14ed5d5cf342d93842647013bb67081124c51965
Matheus Rodrigues <matheus.silva47@estudante.ifms.edu.br>
2025-07-03 20:33:04 -0300

    Update exer05.c

--- exer05.c
+++ exer05.c
@@ -4,11 +4,10 @@ int main(){
 
 float n1, n2, n3;
 
-n1 = 10.1;
-n2 = 9.2;
-n3 = 8.3;
+printf("Digite tres numeros reais para uma nota de um estudante: ");
+scanf("%f %f %f" , &n1, n2, n3);
 
-printf("%f %f %f" , n1, n2, n3);
+printf("%.2f %.2f %.2f" , n1, n2, n3);
 
 return 0;
 }
