1c99c11ab36c9bae99157957752a6d0f9e02f3fa
Matheus Rodrigues <matheus.silva47@estudante.ifms.edu.br>
2025-07-03 20:34:45 -0300

    Update exer09.c

--- exer09.c
+++ exer09.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+  int main(){ 
+  float altura;
+  char genero;
+  
+  printf("Digite sua altura: \n");
+  scanf("%f" , &altura);
 
-int main(){
+  printf("Agora seu genero (M/F): ")
+  scanf("%c" , &genero);
+  
+  if(genero == 'M' || genero == 'F'){
+  printf("%c" , genero);}
 
-float g;
-char h;
-
-g = 1.0;
-h = 'A';
-
-printf("%f %c" , g, h);
+  printf("Sua altura eh: %.2f\n Seu genero eh: %c\n" , altura, genero);
 
 return 0;
 }
