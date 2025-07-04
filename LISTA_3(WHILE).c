6aea091a587e0d13a6e81e33e5f4ae97e2fcc292
Matheus Rodrigues <matheus.silva47@estudante.ifms.edu.br>
2025-07-03 21:32:35 -0300

    Update LISTA_3(WHILE).c

--- LISTA_3(WHILE).c
+++ LISTA_3(WHILE).c
@@ -69,7 +69,7 @@
         float n1, n2, n3;
 
         printf("Digite tres numeros reais para uma nota de um estudante: ");
-        scanf("%f %f %f" , &n1, n2, n3);
+        scanf("%f %f %f" , &n1, &n2, &n3);
 
         printf("%.2f %.2f %.2f" , n1, n2, n3);
       break;        
